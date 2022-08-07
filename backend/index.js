4a7a3cdaedfb20039563fc2a677732992c89c09f
Samarth Chandna <samarthchandna52@gmail.com>
2022-08-07 19:27:12 +0400

    added log and config files

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+const http = require('http')
+const app = require('./app')
+const { PORT } = require('./utils/config')
+const logger = require('./utils/logger')
+
+const server = http.createServer(app)
+server.listen(() => {
+  logger.info(`server is running on port ${PORT}`)
+})
